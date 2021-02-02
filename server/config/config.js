c684fb2970114991268a954440c4139028cd2cfa
Leonardo <ljcl79@gmail.com>
2021-02-02 06:47:04 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,23 @@
 // Puerto
 // =============================
 process.env.PORT = process.env.PORT || 3000;
+
+// =============================
+// Entorno
+// =============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// =============================
+// Base de datos
+// =============================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://ljcl79:Kiplin01%2a@cluster0.2geak.mongodb.net/cafe?authSource=admin&replicaSet=atlas-zml93g-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -9,23 +11,30 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+app.use(require('./routes/usuario'));
  
 app.get('/', function (req, res) {
   res.json('Hello World')
 });
 
-app.get('/usuario', function (req, res) {
-  res.json('getUsuario')
+//Conexión a la base de datos
+let conn = null;
+//clave: WQDi3Ug3bK4YtZl5
+//usuario: ljcl79
+mongoose.connect(process.env.URLDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(function (){
+  conn = true;
+  console.log('BASE ONLINE');
+}).catch(function(err) {
+  console.log(err);
 });
 
-app.post('/usuario', function (req, res) {
-  let body = req.body;
-
-  res.json({persona:body});
-
-});
-
- 
+console.log(conn);
  
 app.listen(process.env.PORT,() => {
     console.log('Escuchando el puerto: ',process.env.PORT);
